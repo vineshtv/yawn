ddcd009d8af6a115b3534aa784f866ca8926c733
vineshtv <tv.vinesh@gmail.com>
2024-10-07 16:57:53 +1100

    Added version

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,20 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "yawn",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "yet another wacky notes tool",
+	Long:  `yawn - Yet Another Wacky Notes tool`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,16 +26,16 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.yawn.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the yawn version",
+	Long:  `Even yawn has versions. This command will reveal it.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.0")
+	},
 }
 
-
+func init() {
+	// Add version
+	rootCmd.AddCommand(versionCmd)
+}
