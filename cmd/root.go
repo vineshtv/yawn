263d4bf3ed6e86e9d087cca42d0b8fbf11629464
vineshtv <tv.vinesh@gmail.com>
2024-10-13 15:05:00 +1100

    Using viper to read a config file.

--- cmd/root.go
+++ cmd/root.go
@@ -8,14 +8,19 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "yawn",
-	Short: "yet another wacky notes tool",
-	Long:  `yawn - Yet Another Wacky Notes tool`,
-}
+var (
+	// config file.
+	cfgFile string
+	// rootCmd represents the base command when called without any subcommands
+	rootCmd = &cobra.Command{
+		Use:   "yawn",
+		Short: "yet another wacky notes tool",
+		Long:  `yawn - Yet Another Wacky Notes tool`,
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -36,6 +41,34 @@ var versionCmd = &cobra.Command{
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Config file (default is $HOME/.yawn.toml)")
 	// Add version
 	rootCmd.AddCommand(versionCmd)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use the config file passed in the flag
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Read config file from default location
+
+		// Find the home directory
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Set config file in home directory
+		viper.AddConfigPath(home)
+		viper.SetConfigType("toml")
+		viper.SetConfigName(".yawn")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println("Error reading config file:", err)
+	}
 }
